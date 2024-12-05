29c391802950e2b2a26ad4466e3c312e1a79bcd5
Rabiu Ahmad O. <rabiu2993@gmail.com>
2024-12-05 10:05:52 +0100

    Fix: Logo not toggling on color mode.

---
+++ src/components/theahmad.tsx
@@ -0,0 +1,13 @@
+import Dark from "./ui/dark";
+import Light from "./ui/light";
+
+const theahmad = () => {
+  return (
+    <>
+      <Dark />
+      <Light />
+    </>
+  );
+};
+
+export default theahmad;
