9eddeafbbbf700a46659eeafd104b47deefe022f
Ahmad <rabiu2993@gmail.com>
2025-10-10 20:36:35 +0100

    Fix: blog posts and projects listing page

--- src/components/Blog.tsx
+++ src/components/Blog.tsx
@@ -13,9 +13,9 @@ export default function Blog({ data, tags }: Props) {
   const [posts, setPosts] = createSignal<CollectionEntry<"blog">[]>([])
 
   createEffect(() => {
-    setPosts(data.filter((entry) => 
-      Array.from(filter()).every((value) => 
-        entry.data.tags.some((tag:string) => 
+    setPosts(data.filter((entry) =>
+      Array.from(filter()).every((value) =>
+        entry.data.tags.some((tag: string) =>
           tag.toLowerCase() === String(value).toLowerCase()
         )
       )
@@ -23,9 +23,9 @@ export default function Blog({ data, tags }: Props) {
   })
 
   function toggleTag(tag: string) {
-    setFilter((prev) => 
-      new Set(prev.has(tag) 
-        ? [...prev].filter((t) => t !== tag) 
+    setFilter((prev) =>
+      new Set(prev.has(tag)
+        ? [...prev].filter((t) => t !== tag)
         : [...prev, tag]
       )
     )
@@ -35,13 +35,19 @@ export default function Blog({ data, tags }: Props) {
     <div class="grid grid-cols-1 sm:grid-cols-3 gap-6">
       <div class="col-span-3 sm:col-span-1">
         <div class="sticky top-24">
-          <div class="text-sm font-semibold uppercase mb-2 text-black dark:text-white">Filter</div>
+          <div class="text-sm font-semibold uppercase mb-2">Filter</div>
           <ul class="flex flex-wrap sm:flex-col gap-1.5">
             <For each={tags}>
               {(tag) => (
                 <li>
-                  <button onClick={() => toggleTag(tag)} class={cn("w-full px-2 py-1 rounded", "whitespace-nowrap overflow-hidden overflow-ellipsis", "flex gap-2 items-center", "bg-black/5 dark:bg-white/10", "hover:bg-black/10 hover:dark:bg-white/15", "transition-colors duration-300 ease-in-out", filter().has(tag) && "text-black dark:text-white")}>
-                    <svg class={cn("size-5 fill-black/50 dark:fill-white/50", "transition-colors duration-300 ease-in-out", filter().has(tag) && "fill-black dark:fill-white")}>
+                  <button
+                    onClick={() => toggleTag(tag)}
+                    class={cn(
+                      "w-full px-2 py-1 rounded",
+                      "whitespace-nowrap overflow-hidden overflow-ellipsis",
+                      "flex gap-2 items-center", "bg-big/5", "hover:bg-big/10", filter().has(tag))}
+                  >
+                    <svg class={cn("size-5 fill-big/50", filter().has(tag))}>
                       <use href={`/ui.svg#square`} class={cn(!filter().has(tag) ? "block" : "hidden")} />
                       <use href={`/ui.svg#square-check`} class={cn(filter().has(tag) ? "block" : "hidden")} />
                     </svg>
